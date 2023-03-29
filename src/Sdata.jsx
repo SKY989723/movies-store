74c73325b9b9fa63e75fb502e9b2345f5c92bf34
Akash Vaishnav <104577018+SKY989723@users.noreply.github.com>
2023-03-29 22:50:43 +0530

    first commit

---
+++ src/Sdata.jsx
@@ -0,0 +1,25 @@
+const Sdata=[
+    {imgsrc:"https://occ-0-4857-2164.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABeJS8fp-Pty_LeKOpX-5ViMUyz2MvbCX4vJCblq3Js4C558mk5ffq0P-LpDAtSVGkR1HJCOfXtGj5dkGdLCXzwXFHFbWip-V7oIu3Jawe9xyB8qpQDj1NWdLfPD5hbckXW4v.jpg?r=4e8",
+    sname:'The Elephant ',
+    title:"A Netflix Orignal series",
+    link:"https://www.netflix.com/in/title/81312835",},
+    { imgsrc:"https://occ-0-4857-2164.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABcbD0mkbPc-th_EMlnWLDribHW4ZXBrVmvY3mvNGikhRdXd9tCWasQPwkF7myxoBS7eNN5LjNFoj0RWZJXEoseA7Ve19bZWa8T8.jpg?r=6e4",
+    sname:'Doctor G',
+    title:"A Netflix Orignal series",
+    link:"https://www.netflix.com/in/title/81350339",},
+    {imgsrc:"https://occ-0-4857-2164.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABY-hEFPD6A75T0bGz5LHoA-dgm7Jj9gOQ54sO06CwUf_XrGaszGv2uLeU02LHqGWL1kFwQTkdU_ZNG6jo1d9wGNZ0xn32juuwZM.jpg?r=de4",
+    sname:'RRR(Hindi)',
+    title:"A Netflix Orignal series",
+    link:"https://www.netflix.com/in/title/81476453",},
+    {imgsrc:"https://occ-0-4857-2164.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABY33FjlYydrZmTddrkmVXH1JV797xiEIzIIM-BLMAPSyfVr3UM5Xx6WlkH-mtMqy1wVn7fpz5TbFggnldL-Ym0N89wHHuk62ov4.jpg?r=f02",
+    sname:'Major(Hindi)',
+    title:"A Netflix Orignal series",
+    link:"https://www.netflix.com/in/title/81242574",},
+    {imgsrc:"https://occ-0-4857-2164.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABRFhZ5DEMlsNrloisG4lNMuiS6-u8eMaOtDLmb7V6_uf94qAF71k46iaX44TBn_SHMiY-kXxMEhBwrHdh2m1TE6qhxHSt5m7CFw.jpg?r=fbd",
+    sname:'Kantara(Hindi)',
+    title:"A Netflix Orignal series",
+    link:"https://www.netflix.com/in/title/81656709",},
+    
+]
+
+export default Sdata;
---
+++ src/index.js
@@ -0,0 +1,79 @@
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Card from './Card';
+import './index.css';
+import Sdata from "./Sdata";
+// console.log(Sdata[0]);
+
+
+// function ncard(value aa jayrgi jo bhe arry ki hogi,index number,array)
+// function ncard(val){
+//   console.log(val);
+// return (
+//   <Card 
+// imgsrc={val.imgsrc}
+//   sname={val.sname}
+//   title={val.title}
+//   link={val.link}
+// />
+// )
+// }
+
+ReactDOM.render(<>
+<h1 className='heading_style'>Top 5 Netflix movies in 2023</h1>
+{/* <Card 
+imgsrc={Sdata[0].imgsrc}
+  sname={Sdata[0].sname}
+  title={Sdata[0].title}
+  link={Sdata[0].link}
+/> */}
+
+{/* {Sdata.map(ncard)} */}
+
+{Sdata.map((val)=>{
+  console.log(val);
+return (
+  <Card 
+imgsrc={val.imgsrc}
+  sname={val.sname}
+  title={val.title}
+  link={val.link}
+/>
+)
+})}
+
+{/* <Card 
+imgsrc={Sdata[1].imgsrc}
+  sname={Sdata[1].sname}
+  title={Sdata[1].title}
+  link={Sdata[1].link}
+/>
+<Card 
+imgsrc={Sdata[2].imgsrc}
+  sname={Sdata[2].sname}
+  title={Sdata[2].title}
+  link={Sdata[2].link}
+/>
+<Card imgsrc={Sdata[2].imgsrc}
+  sname={Sdata[2].sname}
+  title={Sdata[2].title}
+  link={Sdata[2].link}
+/>
+<Card imgsrc={Sdata[0].imgsrc}
+  sname={Sdata[0].sname}
+  title={Sdata[0].title}
+  link={Sdata[0].link}
+/> */}
+
+</>,document.getElementById('root'));
+
+
+// fet arrow function
+// function myname(a,b){
+  // return a+b;
+// }
+// const myname=(a,b)=>{
+  // return a+b;
+// }
